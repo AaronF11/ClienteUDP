382a4ad80766d7f00badbe0947bc44e643a85372
AaronF11 <aaron.flores7908@alumnos.udg.mx>
2024-10-13 14:13:39 -0600

    Agregar archivos de proyecto.

---
+++ Class/ClienteUDP.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClienteUDP.Class
+{
+    /// <summary>
+    /// Clase que representa un cliente UDP.
+    /// </summary>
+    public class Cliente
+    {
+        private UdpClient _clienteUDP;
+        private IPEndPoint _puntoDeConexionRemoto;
+
+        /// <summary>
+        /// Constructor que inicializa el cliente UDP con la dirección IP y el puerto del servidor.
+        /// </summary>
+        /// <param name="direccionIP">Dirección IP del servidor.</param>
+        /// <param name="puerto">Puerto del servidor.</param>
+        public Cliente(string direccionIP, int puerto)
+        {
+            _clienteUDP = new UdpClient();
+            _puntoDeConexionRemoto = new IPEndPoint(IPAddress.Parse(direccionIP), puerto);
+        }
+
+        /// <summary>
+        /// Envía un mensaje al servidor.
+        /// </summary>
+        /// <param name="mensaje">Mensaje a enviar.</param>
+        public void EnviarMensaje(string mensaje)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(mensaje);
+            _clienteUDP.Send(bytes, bytes.Length, _puntoDeConexionRemoto);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ClienteUDP.Class;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClienteUDP
+{
+    /// <summary>
+    /// Ventana principal de la aplicación.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Cliente _clienteUDP;
+
+        /// <summary>
+        /// Constructor que inicializa la ventana principal.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            string direccionIP = "127.0.0.1"; // Dirección IP del localhost
+            int puerto = 6789; // Puerto del servidor
+            _clienteUDP = new Cliente(direccionIP, puerto);
+        }
+
+        /// <summary>
+        /// Manejador del evento Click del botón Enviar Mensaje.
+        /// </summary>
+        /// <param name="sender">Objeto que desencadenó el evento.</param>
+        /// <param name="e">Argumentos del evento.</param>
+        private void EnviarMensajeButton_Click(object sender, RoutedEventArgs e)
+        {
+            string mensaje = MessageTextBox.Text;
+            _clienteUDP.EnviarMensaje(mensaje);
+        }
+    }
+}
